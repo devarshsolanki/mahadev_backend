e61578d83338d60b9a25ad02ce238c7cd6cd40a9
devarsh Solanki <devarshsolanki1625@gmail.com>
2026-03-20 13:37:25 +0530

    Update allowed origins for CORS configuration

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(helmet());
 // CORS configuration
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(',')
-  : ['http://localhost:3000', 'http://localhost:5173',"https://mahadevgroceries.vercel.app"];
+  :['https://mahadevmart.co.in', 'https://www.mahadevmart.co.in',"https://mahadevgroceries.vercel.app"];
 
 app.use(cors({
   origin: (origin, callback) => {
